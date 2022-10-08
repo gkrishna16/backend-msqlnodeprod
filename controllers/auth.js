7184aa931f0047116c0d1b785f1286cf80ab58ef
Gopal Krishan <gkrishna160@gmail.com>
2022-10-08 00:49:07 -0400

    removed consoles

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,7 +51,7 @@ const login = (req, res) => {
 
     res
       .cookie("access_token", token, {
-        httpOnly: true,
+        httpOnly: false,
       })
       .status(200)
       .json(other);
--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const corsOptions = {
   "access-control-allow-credential": true,
 };
 app.use(cors(corsOptions));
-
 app.use(cookieParser());
 
 require("dotenv").config();
@@ -20,7 +19,6 @@ const PORT = process.env.PORT || 5001;
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const posts = require("./routes/posts");
-// const { Model } = require("sequelize");
 
 app.use("/api/users/", users);
 app.use("/api/auth", auth);
