568af732f6250b89b69d22ef6c858376c15dedb5
Gopal Krishan <gkrishna160@gmail.com>
2022-10-07 22:15:48 -0400

    added headers: {'Access-Control-Allow-Origin': '*'}

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
 app.use(cors());
-
+app.use((req, res, next) =>{
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type,Accept, Authortization');  
+res.setHeader('Acces-Control-Allow-Methods','GET, POST, PATCH, DELETE');
 app.use(cookieParser());
 
 require("dotenv").config();
